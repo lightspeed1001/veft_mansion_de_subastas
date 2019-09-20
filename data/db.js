e0aad0955ee704e50b48c639b43226764d600ce8
eytor <eythoras16@ru.is>
2019-09-20 19:29:36 +0000

    new database link

--- data/db.js
+++ data/db.js
@@ -5,7 +5,7 @@ const auctionSchema = require('../schemas/auction');
 const auctionBidSchema = require('../schemas/auctionBid');
 const customerSchema = require('../schemas/customer');
 
-const connection = mongoose.createConnection('mongodb+srv://VeftHopur:Lykilord@clusterveft-dwz26.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true });
+const connection = mongoose.createConnection('mongodb+srv://VeftHopur:Lykilord@clusterveft-dwz26.mongodb.net/mansion_de_subastas', { useNewUrlParser: true });
 
 module.exports = {
     Art: connection.model('Art', artSchema),
