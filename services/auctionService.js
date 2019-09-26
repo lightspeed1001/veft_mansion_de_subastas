c74aa4931e4d7b44a672ecfcf28e7b387fe1f00f
eytor <eythoras16@ru.is>
2019-09-26 21:07:32 +0000

    foiced some errors

--- services/auctionService.js
+++ services/auctionService.js
@@ -7,7 +7,7 @@ const auctionService = () => {
   const getAllAuctions = (cb, errorCb) => {
     Auction.find({}, function(err, auctions) {
       if (err) {
-        errorCb(500, err);
+        errorCb(500, 'Internal database error');
       } else {
         cb(auctions);
       }
@@ -17,9 +17,9 @@ const auctionService = () => {
   const getAuctionById = (id, cb, errorCb) => {
     Auction.findById(id, function(err, auction) {
       if (err) {
-        errorCb(500, err)
+        errorCb(500, err);
       } else if (auction === null) {
-        errorCb(404, err);
+        errorCb(404, 'Auction not fount');
       } else {
         cb(auction);
       }
@@ -29,26 +29,19 @@ const auctionService = () => {
   const getAuctionWinner = (auctionId, cb, errorCb) => {
     Auction.findById(auctionId, function(err, auction) {
       if (err) {
-          console.log('error in find auction');
-        errorCb(500, err)
+        errorCb(500, err);
       } else if (auction === null) {
-          console.log('no auction found');
-        errorCb(404, err);
+        errorCb(404, 'Auction not found');
       } else if (new Date(auction.endDate) > new Date()) {
-          console.log('auction has not ended yet');
         errorCb(409, err);
       } else {
-        console.log('found auction: ', auction);
         Customer.findById(auction.auctionWinner, function(err, customer) {
           if (err) {
-              console.log('error in find customer');
-              errorCb(500, err)
+            errorCb(500, err);
           } else if (customer === null) {
-              console.log('customer not found');
-              errorCb(404, err);
+            cb('This auction had no bids.');
           } else {
-              console.log('customer found: ', customer);
-              cb(customer);
+            cb(customer);
           }
         });
       }
@@ -57,85 +50,147 @@ const auctionService = () => {
 
   const createAuction = (auction, cb, errorCb) => {
     Art.findById(auction.artId, function(err, art) {
-        if (err) {
-            errorCb(500, err);
-        } else if(art === null) {
-            errorCb(400, err);
-        } else if(!art.isAuctionItem) {
-            errorCb(412, err);
-        } else {
+      if (err) {
+        errorCb(500, 'Iternal database error');
+      } else if (art === null) {
+        errorCb(400, 'Art not found');
+      } else if (!art.isAuctionItem) {
+        errorCb(412, 'Selected art is not an auction item');
+      } else {
+        Auction.find({ artId: auction.artId }, function(err, auction) {
+          if (err) {
+            errorCb(500, 'Iternal database error');
+          }
+          if (auction === null || new Dare(auction.endDate) < new Date()) {
             Auction.create(auction, function(err, auction) {
-                if (err) {
-                    errorCb(500, err);
-                } else {
-                  cb(auction);
-                }
+              if (err) {
+                errorCb(500, 'Iternal database error');
+              } else {
+                cb();
+              }
             });
-        }
+          } else {
+            errorCb(
+              409,
+              'There already is a current auction with the same art'
+            );
+          }
+        });
+      }
     });
   };
 
   const getAuctionBidsWithinAuction = (auctionId, cb, errorCb) => {
-    Bids.find({auctionId: auctionId}, function(err, bids) {
-      if(err) errorCb(404, err);
-      else cb(bids)
-    })
+    Bids.find({ auctionId: auctionId }, function(err, bids) {
+      if (err) {
+        errorCb(500, 'Internal database error');
+      } else {
+        cb(bids);
+      }
+    });
   };
 
-  function saveBidWrapper(auction, customerId, cb, errorCb){
+  function saveBidWrapper(auction, customerId, cb, errorCb) {
     return function(err, bid) {
-      if(err) errorCb(500, err);
-      else 
-      {
+      if (err) {
+        errorCb(500, 'Internal database error');
+      } else {
         auction.auctionWinner = customerId;
         auction.save(function cb(err) {
-          if(err) errorCb(500, err);
+          if (err) errorCb(500, 'Internal database error');
           else cb(bid);
         });
       }
-    }
+    };
   }
 
-  function processBidsWrapper(auction, auctionId, customerId, price, cb, errorCb) {
-    return function processBids (bids) {
+  function processBidsWrapper(
+    auction,
+    auctionId,
+    customerId,
+    price,
+    cb,
+    errorCb
+  ) {
+    return function processBids(bids) {
       maxBid = 0;
       bids.forEach(element => {
-        if(element.price >= maxBid) maxBid = element.price;
+        if (element.price >= maxBid) maxBid = element.price;
       });
-      
-      if(maxBid >= price) errorCb(412, null);
-      else {  
-        Bids.create({auctionId: auctionId, customerId:customerId, price:price}, saveBidWrapper(auction, customerId, cb, errorCb));
-      }
-    }
-  }
 
-  function placeBidWithCustomerWrapper (auction, auctionId, customerId, price, cb, errorCb) {
-    return function placeBidWithCustomer (err, customer) {
-      if(customer === null) errorCb(400, err);
-      else if(err) errorCb(400, err)
+      if (maxBid >= price) errorCb(412, null);
       else {
-        getAuctionBidsWithinAuction(auctionId, 
-          processBidsWrapper(auction, auctionId, customerId, price, cb, errorCb), 
-          function(err){ errorCb(404, err); }
+        Bids.create(
+          { auctionId: auctionId, customerId: customerId, price: price },
+          saveBidWrapper(auction, customerId, cb, errorCb)
         );
       }
-    }
+    };
   }
 
-  function placeBidWithAuctionWrapper (auctionId, customerId, price, cb, errorCb) {
-    return function pladeBidWithAuction (err, auction) {
-      if(err) errorCb(404, err);
-      else if(auction === null) errorCb(404, err);
-      else if(auction.minimumPrice > price) errorCb(412, err);
-      else if(new Date(auction.endDate) < new Date()) errorCb(403, err);
-      else Customer.findById(customerId, placeBidWithCustomerWrapper(auction, auctionId, customerId, price, cb, errorCb));
-    }
+  function placeBidWithCustomerWrapper(
+    auction,
+    auctionId,
+    customerId,
+    price,
+    cb,
+    errorCb
+  ) {
+    return function placeBidWithCustomer(err, customer) {
+      if (customer === null) errorCb(400, err);
+      else if (err) errorCb(400, err);
+      else {
+        getAuctionBidsWithinAuction(
+          auctionId,
+          processBidsWrapper(
+            auction,
+            auctionId,
+            customerId,
+            price,
+            cb,
+            errorCb
+          ),
+          function(err) {
+            errorCb(404, err);
+          }
+        );
+      }
+    };
+  }
+
+  function placeBidWithAuctionWrapper(
+    auctionId,
+    customerId,
+    price,
+    cb,
+    errorCb
+  ) {
+    return function pladeBidWithAuction(err, auction) {
+      if (err) errorCb(404, err);
+      else if (auction === null) errorCb(404, err);
+      else if (auction.minimumPrice > price) errorCb(412, err);
+      else if (new Date(auction.endDate) < new Date()) errorCb(403, err);
+      else
+        Customer.findById(
+          customerId,
+          placeBidWithCustomerWrapper(
+            auction,
+            auctionId,
+            customerId,
+            price,
+            cb,
+            errorCb
+          )
+        );
+    };
   }
 
   const placeNewBid = (auctionId, customerId, price, cb, errorCb) => {
     // Make sure auctionId and customerId are valid
-    Auction.findById(auctionId, placeBidWithAuctionWrapper(auctionId, customerId, price, cb, errorCb));
+    Auction.findById(
+      auctionId,
+      placeBidWithAuctionWrapper(auctionId, customerId, price, cb, errorCb)
+    );
   };
 
   return {
